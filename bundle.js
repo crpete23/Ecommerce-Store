77755ebf5c59df3be752446254d05cb3759f1297
jewell86 <jewellsmailbox@gmail.com>
2018-06-19 08:23:48 -0700

    added email validation and shake

--- bundle.js
+++ bundle.js
@@ -1,4 +1,22 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
 
+const emailInput = document.querySelector(".email-input")
+const emailForm = document.querySelector(".email-form")
+const submitButton = document.querySelector(".submit-button")
+
+submitButton.addEventListener('click', function(event) {
+  console.log('hello');
+    if(emailInput.value === "") {
+      emailInput.classList.add("shake-box")
+      event.preventDefault()
+        setTimeout(function() {
+          emailInput.classList.remove("shake-box")
+        }, 300)
+    }else{
+      alert("Thank you for signing up!")
+    }
+
+})
+
 },{}]},{},[1]);
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
 
+
+const emailInput = document.querySelector(".email-input")
+const emailForm = document.querySelector(".email-form")
+const submitButton = document.querySelector(".submit-button")
+
+submitButton.addEventListener('click', function(event) {
+  console.log('hello');
+    if(emailInput.value === "") {
+      emailInput.classList.add("shake-box")
+      event.preventDefault()
+        setTimeout(function() {
+          emailInput.classList.remove("shake-box")
+        }, 300)
+    }else{
+      alert("Thank you for signing up!")
+    }
+
+})
