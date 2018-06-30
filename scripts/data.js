7a8044380e65384f5c2d64f3eb47ed214c859e0f
dtruong11 <dtruong11@users.noreply.github.com>
2018-06-29 19:59:51 -0700

    accomplished filtering

--- scripts/data.js
+++ scripts/data.js
@@ -1,21 +1,125 @@
 //This will hold ALL of our products. We could maybe add a key:value pair that is { filter: 'morty'} or
 //something to grab each product with the filter function
 
-const products = [
-  { title: '1 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '2 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '3 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '4 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '5 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '6 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '7 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '8 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '9 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '10 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '11 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '12 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '13 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
-  { title: '14 Interdimensional Cable Box', image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523", price: '$100', mortys: '5 mortys', reviews: '10 reviews' },
+const products = [{
+    title: 'Microsverse Battery',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/8/86/Microverse_Battery.png/revision/latest?cb=20160910010946",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'tech',
+    sale: true
+  },
+  {
+    title: 'Mr.Meeseeks Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/f/f7/Mr._Meeseeks_Box.png/revision/latest?cb=20160909153718",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'tech',
+    sale: true
+  },
+  {
+    title: '3 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'morty',
+    sale: false
+  },
+  {
+    title: '4 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'tech',
+    sale: false
+  },
+  {
+    title: '5 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'morty',
+    sale: true
+  },
+  {
+    title: '6 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'rick',
+    sale: false
+  },
+  {
+    title: '7 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'tech',
+    sale: true
+  }, {
+    title: '1 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'tech',
+    sale: true
+  }, {
+    title: '2 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'rick',
+    sale: true
+  }, {
+    title: '3 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'morty',
+    sale: false
+  }, {
+    title: '4 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'tech',
+    sale: false
+  }, {
+    title: '5 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'morty',
+    sale: true
+  }, {
+    title: '6 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'rick',
+    sale: false
+  }, {
+    title: '7 Interdimensional Cable Box',
+    image: "https://vignette.wikia.nocookie.net/rickandmorty/images/2/27/Interdimensional_Cable_Box.png/revision/latest?cb=20160910005523",
+    price: '$100',
+    mortys: '5 mortys',
+    reviews: '10 reviews',
+    type: 'tech',
+    sale: true
+  }
 ]
 
 // console.log(products[0].image);
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const toggleAddCart = require ("./product-to-cart.js")
+const toggleAddCart = require("./product-to-cart.js")
 const data = require('./data')
 const populateProducts = require('./templates')
 
@@ -8,31 +8,72 @@ const emailInput = document.querySelector('.email-input')
 const emailForm = document.querySelector('.email-form')
 const submitButton = document.querySelector('.submit-button')
 
-
-//So I had to disable the submit.addEventListener bc this element doesn't exist on the products page.
-//I couldn't get the below function to run from the new populate-products.js page. I am not good with browserify
-//and I think maybe our products.html page should not use it because we don't necessarily want the entire main.js
-//file running on our products.html page? Help haha.
-//
-
 //PRODUCT POPULATION FUNCTION USING TEMPLATE AND DATA
-function renderFilteredProducts(products){
+function renderFilteredProducts(products) {
+  document.querySelector('.products-display').innerHTML = ''
   let filteredProductsArray = products.map(populateProducts)
-  let productsDisplayTable = document.querySelector('.products-page-container')
-  let rowsAmount = Math.round(filteredProductsArray.length/3)
+  let productsDisplayTable = document.querySelector('.products-display')
+  let rowsAmount = Math.round(filteredProductsArray.length / 3)
   let counter = 0
 
-  for(let i = 0; i < rowsAmount; i++){
+
+  for (let i = 0; i < rowsAmount; i++) {
     let row = document.createElement('row')
     productsDisplayTable.append(row)
     //adds row to page
-      for(let j = 0; j < 3; j++){
-        if(counter < filteredProductsArray.length){
+    for (let j = 0; j < 3; j++) {
+      if (counter < filteredProductsArray.length) {
         let product = filteredProductsArray[counter]
         row.innerHTML += product
-        counter ++
-        }
-     }
+        counter++
+      }
+    }
   }
 }
 renderFilteredProducts(data)
+
+const links = Array.from(document.querySelectorAll('.product-listing .sort ul li a'))
+
+links.map(el => el.addEventListener('click', function() {
+  const dataValue = el.dataset.category
+  if (dataValue === 'all') {
+    renderFilteredProducts(data)
+  } else if (dataValue === 'rick') {
+    renderRick(data)
+  } else if (dataValue === 'morty') {
+    renderMorty(data)
+  } else if (dataValue === 'tech') {
+    renderTech(data)
+  } else if (dataValue === 'sale') {
+    renderSale(data)
+  } else {
+    "There is some error here"
+  }
+}))
+
+
+function renderRick(data) {
+  const rickDataArr = data.filter(el => {
+    return el.type === 'rick'
+  })
+  renderFilteredProducts(rickDataArr)
+}
+
+function renderMorty(data) {
+  const mortyDataArr = data.filter(el => {
+    return el.type === 'morty'
+  })
+  renderFilteredProducts(mortyDataArr)
+}
+
+function renderTech(data) {
+  const techDataArr = data.filter(el => {
+    return el.type === 'tech'
+  })
+  renderFilteredProducts(techDataArr)
+}
+
+function renderSale(data) {
+  const saleDataArr = data.filter(el => el.sale)
+  renderFilteredProducts(saleDataArr)
+}
